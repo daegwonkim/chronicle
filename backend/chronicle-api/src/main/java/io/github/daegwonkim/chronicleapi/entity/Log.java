8e0ea35c2d1aebd609bfde6a6b256f2c386be96b
kimdaegwon <daegwonkim.dev@gmail.com>
2026-01-26 23:17:51 +0900

    feat: 로그 저장 API 구현 - Controller 레이어 구현 - Service 레이어 구현 - Repository 레이어 구현 - JPA 의존성 추가

---
+++ backend/chronicle-api/src/main/java/io/github/daegwonkim/chronicleapi/entity/Log.java
@@ -0,0 +1,41 @@
+package io.github.daegwonkim.chronicleapi.entity;
+
+import io.github.daegwonkim.chronicleapi.enumerate.LogLevel;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "logs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Log {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "app_id", nullable = false, updatable = false)
+    private Long appId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, updatable = false)
+    private LogLevel level;
+
+    @Column(updatable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "logged_at", nullable = false, updatable = false)
+    private Instant loggedAt;
+
+    public static Log create(Long appId, LogLevel level, String message, Instant loggedAt) {
+        Log log = new Log();
+
+        log.appId = appId;
+        log.level = level;
+        log.message = message;
+        log.loggedAt = loggedAt;
+
+        return log;
+    }
+}
---
+++ backend/chronicle-api/src/main/java/io/github/daegwonkim/chronicleapi/enumerate/LogLevel.java
@@ -0,0 +1,5 @@
+package io.github.daegwonkim.chronicleapi.enumerate;
+
+public enum LogLevel {
+    TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+}
